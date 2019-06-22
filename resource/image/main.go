60bd8e663367414291d11da930cd5610b88ece84
Junpei Tsuji <tsuji_jumpei@cyberagent.co.jp>
2019-06-22 14:55:09 +0900

    Check goroutine

--- resource/image/main.go
+++ resource/image/main.go
@@ -10,9 +10,22 @@ func main() {
 	cpus := runtime.NumCPU()
 	fmt.Println("CPUs:", cpus)
 
+	for i := 0; i < 100; i++ {
+		goroutine()
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "hello world")
 	})
 
 	http.ListenAndServe(":8000", nil)
+}
+
+func goroutine() {
+	go func() {
+		var counter int64
+		for {
+			counter++
+		}
+	}()
 }
